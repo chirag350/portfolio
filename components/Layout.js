ad340cd725cec4d16e3c33393672b1cf7022fba9
chirag350 <chiraglamba350@gmail.com>
2022-01-11 22:44:32 +0530

    t

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+const Layout = ({ children }) => {
+    return (
+        <div className="next-container">
+          <Head>
+            <title>Eggactyl</title>
+            <meta name="description" content="Eggactyl Admin UI" />
+            <link rel="icon" href="/favicon.png" />
+            <meta charSet="UTF-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
+          <main className="main">
+              {children}
+          </main>
+        </div>
+      )
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+    <h1>Welcome to Chirag's portfolio</h1>
+    <div class="container">
+      <p>Hello World!</p>
+    </div>
+    </>
+  )
+}
