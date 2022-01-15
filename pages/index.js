819997a1869a7abc255cefda7549c4141ad17920
chirag350 <chiraglamba350@gmail.com>
2022-01-16 03:54:19 +0530

    animate pfp

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,14 @@ export default function Home() {
     <>
       <h1>Welcome to Chirag's portfolio</h1>
       <div className="container">
-        <img src="/avatar.png" className="avatar" alt="Avatar" width="100" height="100" />
+        <img src="/rick-astley-never-gonna-give-you-up.gif" className="avatar" alt="Avatar" width="100" height="100" />
         <h2>Hello There. I'm Chirag!</h2>
         <p>Hi! I'm Chirag, A full stack developer from India. I'm currently a student and I specialize in Node.js, Bash, React, Next.js. <br /><br /> Connect with me using the following links:</p>
         <div className="social-links">
           <div className="socials">
             <a href="https://github.com/chirag350"><img src="/github.png" width="50" height="50" className="social" /></a>
             <a href="https://discord.gg/BNcbMACprP"><img src="/discord.png" width="50" height="50" className="social" /></a>
+            <a href="https://steamcommunity.com/id/chiraglamba5/"><img src="/steam.png" width="50" height="50" className="social" /></a>
           </div>
         </div>
       </div>
