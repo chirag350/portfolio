c8a36370f6720c4fd0c424f72caee05e88cda914
chirag350 <chiraglamba350@gmail.com>
2022-01-16 16:05:04 +0530

    change avatar

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <h1>Welcome to Chirag's portfolio</h1>
       <div className="container">
-        <img src="/rick-astley-never-gonna-give-you-up.gif" className="avatar" alt="Avatar" width="100" height="100" />
+        <img src="/favicon.png" className="avatar" alt="Avatar" width="100" height="100" />
         <h2>Hello There. I'm Chirag!</h2>
         <p>Hi! I'm Chirag, A full stack developer from India. I'm currently a student and I specialize in Node.js, Bash, React, Next.js. <br /><br /> Connect with me using the following links:</p>
         <div className="social-links">
