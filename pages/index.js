10bc8dd9cde8c3a3163a6233aad03da09f5227a6
chirag350 <chiraglamba350@gmail.com>
2022-04-30 18:48:35 +0530

    e

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,14 @@ export default function Home() {
         {styles =>
           <animated.div style={styles}>
             <Parallax pages={3} className="text-white bg-background backdrop-blur-xl min-h-screen">
-              <ParallaxLayer offset={0} speed={0.6}>
+              <ParallaxLayer offset={0} speed={0.8}>
                 <div className="p-layer">
                   <h1 className="text-5xl mb-6">Hi! I&apos;m Chirag! </h1>
                   <p>A full stack developer from India. I&apos;m currently a student.</p>
                   <h4 className="text-xl mt-4">Scroll down to read more</h4>
                 </div>
               </ParallaxLayer>
-              <ParallaxLayer offset={1} speed={0.6}>
+              <ParallaxLayer offset={1} speed={0.8}>
                 <div className="p-layer">
                   <h1 className="text-5xl">My Skills</h1>
                   <Skills />
