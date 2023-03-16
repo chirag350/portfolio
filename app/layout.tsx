5e326853f221de1766924bb4f1b31c1c6be20f02
chirag350 <chiraglamba350@gmail.com>
2023-03-16 19:20:39 +0530

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Hi! I am chirag, a fullstack developer from India. Also, I'm currently a student" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Chirag L.</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@900&family=Space+Grotesk&family=Tilt+Neon&display=swap" rel="stylesheet" />
